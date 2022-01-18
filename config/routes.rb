b190641ec487cbf15665754688dd086b343ec978
kage <81172243+kage95@users.noreply.github.com>
2022-01-18 14:44:52 +0000

    [add]商品新規登録ページ

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   devise_for :admin,skip: [:registrations, :passwords],controllers:{
     sessions: "admin/sessions"
   }
+  
   namespace :admin do
     resources :genres
+    resources :items
   end
   
   devise_for :customers,skip: [:passwords,],controllers:{
