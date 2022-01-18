de93c52ef0c3c04df8a29325ac1a7864c167d3fb
kage <81172243+kage95@users.noreply.github.com>
2022-01-18 08:38:26 +0000

    [add]ジャンル一覧ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  root to:"homes#top"
+  get "/admin" => "homes#top"
+  get "homes/about"=> "homes#about",as:"about"
+  devise_for :admin,skip: [:registrations, :passwords],controllers:{
+    sessions: "admin/sessions"
+  }
+  namespace :admin do
+    resources :genres
+  end
+  
+  devise_for :customers,skip: [:passwords,],controllers:{
+     registrations: "public/registrations",
+     sessions: "public/sessions"
+  }
+  
+  resources :items,only:[:index,:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20220117070058_create_order_details.rb
@@ -0,0 +1,13 @@
+class CreateOrderDetails < ActiveRecord::Migration[6.1]
+  def change
+    create_table :order_details do |t|
+      t.integer :order_id,null:false
+      t.integer :items_id,null:false
+      t.integer :tax_inclouded_price,null:false
+      t.integer :count,null:false
+      t.integer :making_status,default: "1"
+
+      t.timestamps
+    end
+  end
+end
