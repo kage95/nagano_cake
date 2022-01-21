ca3de4b3dedb7dc7542d88b529b2309bcdecd798
kage <81172243+kage95@users.noreply.github.com>
2022-01-21 14:03:09 +0000

    [start]aboutページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to:"homes#top"
+  get "homes/about" => "homes#about",as:"about"
   get "/admin" => "homes#top"
   get "homes/about"=> "homes#about",as:"about"
   devise_for :admin,skip: [:registrations, :passwords],controllers:{
