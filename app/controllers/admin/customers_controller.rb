e85a146b4877d80e123a465cde35da57ff879909
kage <81172243+kage95@users.noreply.github.com>
2022-01-24 03:40:27 +0000

    [finish]会員情報編集ページ

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -11,4 +11,16 @@ class Admin::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
+  
+  def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:first_name,:first_name_kana,:last_name,:last_name_kana,:email,:postal_code,:address,:telephone_number)
+  end
 end
