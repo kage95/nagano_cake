d3ebdd3a4c1ad4df9166238f45cb6f22b900c4dc
kage <81172243+kage95@users.noreply.github.com>
2022-01-30 12:24:03 +0000

    [add]footer

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,7 +1,7 @@
 class Public::OrdersController < ApplicationController
   
   def index
-    @orders = current_customer.orders
+    @orders = current_customer.orders.page(params[:page]).order(id: "desc").per(8)
   end
   
   def show
